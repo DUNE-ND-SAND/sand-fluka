79dad7bff02a22c41f4ae476056cf4a3479a47d2
Zahra Ghorbani Moghaddam <Zahra.Ghorbani.Moghaddam@cern.ch>
2020-03-04 17:54:47 +0100

    First implementation of FillTrajectories

--- fluka2edepsim/src/FillTrajectories.h
+++ fluka2edepsim/src/FillTrajectories.h
@@ -1,47 +1,67 @@
-#include "TTree.h"
-
 void FillTrajectories(std::vector<TG4Trajectory>& dest, TTree *HitsTree) {
 	dest.clear();
+        // HeaderTree->GetEntries();  FIXME
+        // ntrack->GetEntries()
+	Double_t PrTrInc = -1;
+        // Making another container to appened the unordered Info
+        std::map<Int_t,Float_t> TrInDest;
+	TG4Trajectory *tx = 0;
+        
 
-	//I need number of tracks
-	int ntracks=10; //HeaderTree->GetEntries();  FIXME
+	for (int j=0; j< NIncHits; j++) {
+                std::cout<< " TrInc : "<<j <<" "<< TrInc[j] <<std::endl;	
+		if(TrInc[j] != PrTrInc){
+                        //------- New
+                        //If it doesnt find it --> add
+                        if(tx != 0 && TrInDest.find(PrTrInc)==TrInDest.end()){
+                        //if(tx != 0){
+                                TrInDest.insert(std::make_pair(PrTrInc,dest.size()));
+				dest.push_back(*tx);
+                                std::cout<<" ************* Track is not there ***********"<<std::endl;
+                                std::cout<<dest.size()<<std::endl;
+				//break;
+                        }
+			PrTrInc = TrInc[j];
+                        //------- New
+                        //If it finds it let it be
+			if(TrInDest.find(TrInc[j])!=TrInDest.end()){
+				tx = &(dest[TrInDest[TrInc[j]]]);
+                                std::cout<<" ************* Track is there ***********"<<std::endl;
+                        //If it doesnt exist make it
+			}else{
+				tx           = new TG4Trajectory;
+				tx->TrackId  = TrInc[j];
+				tx->ParentId = IdParInc[j];
+                                std::cout<< " ********** Track Id ***********"<< tx->TrackId<<std::endl;
+                                std::cout<< " ********** Parent Id ***********"<< tx->ParentId<<std::endl;
+				//tx->Name = "prova";
+				//tx->PDGCode= 10;
 
-	for(int i=0; i<ntracks; i++){
-
-		TG4Trajectory tx;
-
-		tx.TrackId= 2;
-		tx.ParentId = 2;
-		tx.Name = "prova";
-		tx.PDGCode= 10;
-
-		tx.InitialMomentum.SetXYZM(0.9, 0.9, 0.9, 100);
-		//traj.InitialMomentum.SetXYZM(ndTraj->GetInitialMomentum().x(),
-		// ndTraj->GetInitialMomentum().y(),
-		// ndTraj->GetInitialMomentum().z(),
-		// part->GetPDGMass();
-
-		int npoints=4;
-		// Add the particles associated with the vertex to the summary.
-		for (int j=0; j< npoints; j++) {
-
-			//make sure they are ordered...etc ...see code /src/EDepSimPersistencyManager.cc at line 437
-			TG4TrajectoryPoint point;
-			point.Position.SetXYZT(11.0, 11.0, 11.0,11.0);
-			point.Momentum.SetXYZ(12.0, 12.0, 12.0);
-			
-			//point.Position.SetXYZT(edepPoint->GetPosition().x(),
-			//		edepPoint->GetPosition().y(),
-			//		edepPoint->GetPosition().z(),
-			//		edepPoint->GetTime());
-			//point.Momentum.SetXYZ(edepPoint->GetMomentum().x(),
-			//		edepPoint->GetMomentum().y(),
-			//		edepPoint->GetMomentum().z());
-			//point.Process = edepPoint->GetProcessType();
-			//point.Subprocess = edepPoint->GetProcessSubType();
-			tx.Points.push_back(point);
+				// tx ->InitialMomentum.SetXYZM(0.9, 0.9, 0.9, 100);
+				// traj.InitialMomentum.SetXYZM(ndTraj->GetInitialMomentum().x(),
+				// ndTraj->GetInitialMomentum().y(),
+				// ndTraj->GetInitialMomentum().z(),
+				// part->GetPDGMass();
+			}
 		}
+		// Add the particles associated with the vertex to the summary.
+		// Make sure they are ordered...etc ...see code /src/EDepSimPersistencyManager.cc at line 437
+		TG4TrajectoryPoint point;
 
-		dest.push_back(tx);
-	}
+		point.Position.SetXYZT(PosInc[j][0], PInc[j][1], PInc[j][2], TimeInc[j]);
+		point.Momentum.SetXYZ (PInc[j][0], PInc[j][1], PInc[j][2]);
+	        std::cout<< " Pos(x) , Pos(y) , Pos(z) , Time : "<< PosInc[j][0]<<" , "<< PosInc[j][1] <<" , "<<PosInc[j][2]<<" , "<< TimeInc[j]<<std::endl; 
+                std::cout<< " Px  , Py , Pz : "<< PInc[j][0] <<" , "<< PInc[j][1] <<" , "<< PInc[j][2] <<std::endl;	
+		//point.Position.SetXYZT(edepPoint->GetPosition().x(),
+		//		edepPoint->GetPosition().y(),
+		//		edepPoint->GetPosition().z(),
+		//		edepPoint->GetTime());
+		//point.Momentum.SetXYZ(edepPoint->GetMomentum().x(),
+		//		edepPoint->GetMomentum().y(),
+		//		edepPoint->GetMomentum().z());
+		//point.Process = edepPoint->GetProcessType();
+		//point.Subprocess = edepPoint->GetProcessSubType();
+		tx->Points.push_back(point);
+
+       }
 }
--- fluka2edepsim/src/MainWrite.cxx
+++ fluka2edepsim/src/MainWrite.cxx
@@ -1,11 +1,26 @@
-#include "TG4Event.h"
-#include "FillPrimaries.h"
-//#include "FillTrajectories.h"
-#include <iostream>
-#include "TTree.h"
+#include <map>
 #include "TFile.h"
+#include "TG4Event.h"
+//#include "FillPrimaries.h"
+//#include "FillTrajectories.h"
 
-int main() {
+const int MaxNhit   = 10000; //Max Number of hits
+const int MaxNLep   = 10000; //Max Number of hits
+const int MaxNHad   = 10000; //Max Number of hits
+const int MaxNHeavy = 10000; //Max Number of hits
+const int MaxNPhot  = 10000; //Max Number of hits
+const int MaxNTr    = 10000; //Max Number of hits
+//Global variables
+Int_t  NumTracks, IdTrck[MaxNTr], NumLep, IdLep[MaxNLep], TrLep[MaxNLep], NumHad, IdHad[MaxNHad], TrHad[MaxNHad], NumHeavy, IdHeavy[MaxNHeavy], TrHeavy[MaxNHeavy], NumPhot, IdPhot[MaxNPhot], TrPhot[MaxNPhot];
+Float_t P_Lep[MaxNLep]  , P_Had[MaxNHad], P_Heavy[MaxNHeavy], P_Phot[MaxNPhot];
+Int_t   RunNum, EveNum  , NIncHits, IdTrack     , IdInc[MaxNhit], IdParInc[MaxNhit], TrInc[MaxNhit];
+Float_t PInc[MaxNhit][5], TimeInc[MaxNhit];
+Float_t PosInc[MaxNhit][3];
+
+#include "FillPrimaries.h"
+#include "FillTrajectories.h"
+
+int  main() {
 	// check to see if the event class is in the dictionary
 	// if it is not load the definition in libEvent.so
 
@@ -14,8 +29,8 @@ int main() {
 	//gSystem->Load("libEvent.so");
 	//cout<<"ho caricato la libreria"<<endl;
 	// }
-	std::cout<<"Inizio del programma"<<std::endl;
-	
+
+	//da mettere nel .h
 	/// The ROOT output file that events are saved into.
 	TFile *fOutput;
 
@@ -23,14 +38,67 @@ int main() {
 	TTree *fEventTree;
 
 	/// The number of events saved to the output file since the last write.
-	//int fEventsNotSaved;
-
+	int fEventsNotSaved;
+	//FINE .hh
+        
 
 	//Opening FLUKA FILE
 	
 	TFile *fInput = new TFile("/eos/user/s/salap/DUNE-IT/sand/sand_numu001_Out.root");
     	TTree *HeaderTree  = (TTree*)fInput->Get("HeaderTree");
-    	//TTree *HitsTree = (TTree*)f->Get("HitsTree");
+    	TTree *HitsTree = (TTree*)fInput->Get("HitsTree");
+        //HeaderTree Info
+    	//**********************************************
+        HeaderTree->SetBranchAddress("NumTracks",&NumTracks);
+        HeaderTree->SetBranchAddress("IdTrck",&IdTrack);
+        //Lept Info
+        HeaderTree->SetBranchAddress("NumLep",&NumLep);
+        HeaderTree->SetBranchAddress("IdLep",&IdLep);
+        HeaderTree->SetBranchAddress("TrLep",&TrLep);
+        HeaderTree->SetBranchAddress("P_Lep",&P_Lep);
+        //Had Info
+        HeaderTree->SetBranchAddress("NumHad",&NumHad);
+        HeaderTree->SetBranchAddress("IdHad",&IdHad);
+        HeaderTree->SetBranchAddress("TrHad",&TrHad);
+        HeaderTree->SetBranchAddress("P_Had",&P_Had);
+        //Heavy Particle Info
+        HeaderTree->SetBranchAddress("NumHeavy",&NumHeavy);
+        HeaderTree->SetBranchAddress("IdHeavy",&IdHeavy);
+        HeaderTree->SetBranchAddress("TrHeavy",&TrHeavy);
+        HeaderTree->SetBranchAddress("P_Heavy",&P_Heavy);
+        //Photon Info
+        HeaderTree->SetBranchAddress("NumPhot",&NumPhot);
+        HeaderTree->SetBranchAddress("IdPhot",&IdPhot);
+        HeaderTree->SetBranchAddress("TrPhot",&TrPhot);
+        HeaderTree->SetBranchAddress("P_Phot",&P_Phot);
+    	//*************************************************
+        //HitTree Info
+        HitsTree->SetBranchAddress("RunNum",&RunNum);
+        HitsTree->SetBranchAddress("EveNum",&EveNum);
+        HitsTree->SetBranchAddress("NIncHits",&NIncHits);             //Number of hits for each particles , Mc hit
+        HitsTree->SetBranchAddress("IdInc",&IdInc);         //particle ID                       , Mc hit
+        HitsTree->SetBranchAddress("IdParInc",&IdParInc);   //Parent ID                         , MC hit
+        HitsTree->SetBranchAddress("TrInc",&TrInc);         //Track Num                         , MC hit
+        HitsTree->SetBranchAddress("PosInc",&PosInc);    //Position[3]   (x,y,z)             , Mc hit
+        HitsTree->SetBranchAddress("PInc",&PInc);        //Energy-Mom[5] (px,py,pz,E,P)      , Mc hit
+        HitsTree->SetBranchAddress("TimeInc",&TimeInc);     //Time                              , Mc hit
+        /*
+	Int_t fRun,fEvent;
+    	Float_t fx,fy,fz;
+    	T->SetBranchAddress("Run",&Run);
+    	T->SetBranchAddress("Event",&Event);
+    	T->SetBranchAddress("x",&x);
+    	T->SetBranchAddress("y",&y);
+    	T->SetBranchAddress("z",&z);
+    	TF->SetBranchAddress("Run",&fRun);
+    	TF->SetBranchAddress("Event",&fEvent);
+    	TF->SetBranchAddress("x",&fx);
+    	TF->SetBranchAddress("y",&fy);
+    	TF->SetBranchAddress("z",&fz);
+    	T->AddFriend(TF);
+	*/
+
+
 	
 	//Opening EDEPSIM OUTPUT FILE	
 	fOutput = TFile::Open("Provadep.root", "RECREATE", "EDepSim Root Output");
@@ -42,31 +110,37 @@ int main() {
 	TG4Event *pEvent=new TG4Event();
 	fEventTree->Branch("Event","TG4Event",&pEvent);
 
+	fEventsNotSaved = 0;
+
 	int NEVENT=HeaderTree->GetEntries();
 	std::cout<<"Number of event to rewrite: "<<NEVENT<<std::endl;
 	
 	//scrivo dentro EDEPSIM
-	for(int i=0; i<10; i++){  
+	for(int i=0; i<NEVENT; i++){  
 		pEvent->RunId = 0;
 		pEvent->EventId = i;
+		HeaderTree->GetEntry(i);
+		HitsTree->GetEntry(i);
 		std::cout<<"Event for run " << pEvent->RunId	<< " event " << pEvent->EventId<<std::endl;
+                std::cout<< " TrInc : "<< TrInc[0] <<std::endl;	
+                std::cout<< " TrInc : "<< TrInc[2] <<std::endl;	
 
 		// Summarize the trajectories first so that fTrackIdMap is filled.
 		//MarkTrajectories(event);
 		
-		FillPrimaries(pEvent->Primaries, HeaderTree, i);
-		std::cout<<"   Primaries " << pEvent->Primaries.size()<<std::endl;
+		//FillPrimaries(pEvent->Primaries, HeaderTree);
+		//std::cout<<"   Primaries " << pEvent->Primaries.size()<<endl;
 
-	//	FillTrajectories(pEvent->Trajectories,FlukaTree);
+		FillTrajectories(pEvent->Trajectories,HitsTree);
 		std::cout<<"   Trajectories " << pEvent->Trajectories.size()<<std::endl;
 
 		//FillSegmentDetectors(fEventSummary.SegmentDetectors, event);
 		//cout<"   Segment Detectors "	<< pEvent.SegmentDetectors.size()<<endl;
-	fEventTree->Fill();
+		fEventTree->Fill();
+		//break;
 	}
 	fOutput->Write();            // Write the file header
 	fEventTree->Print();           // Print the tree contents
-        fOutput->Close();
 }
 
 
