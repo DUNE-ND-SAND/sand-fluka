9a3edb51c0c8b9f049773bffae427d4a838ea6c7
Zahra Ghorbani Moghaddam <Zahra.Ghorbani.Moghaddam@cern.ch>
2020-04-03 11:20:25 +0200

    Correction to Check.h header file

--- fluka2edepsim/src/Check.h
+++ fluka2edepsim/src/Check.h
@@ -2,10 +2,10 @@
 #define test2
 void  Check(){
 	bool check;
-        std::cout<< "Nhits From hitstree and Stttree: "<< NIncHits<< " , "<< NStt <<std::endl;
-	for(int j=0; j< NIncHits; j++){
+	std::cout<< "Nhits From hitstree and Stttree: "<< NIncHits<< " , "<< NStt <<std::endl;
+	for (int i=0; i< NStt; i++){
 		check = false;
-		for (int i=0; i< NStt; i++){
+		for(int j=0; j< NIncHits; j++){
 			if (TrInc[j] == TrStt[i]) check = true;;
 		}
 		if (check != true) std::cout<<" Track Id is not consistent"<<std::endl;
@@ -13,9 +13,3 @@ void  Check(){
 	}
 }
 #endif
-
-
-
-
-
-
