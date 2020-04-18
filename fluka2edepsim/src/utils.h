d9331afd6a2bb67211aedd309a50757b8656bb84
leadinoto <lea.dinoto@ge.infn.it>
2020-04-18 21:58:49 +0000

    unit of meas in mm for Matteo

--- fluka2edepsim/src/utils.h
+++ fluka2edepsim/src/utils.h
@@ -3,9 +3,9 @@
 
 TLorentzVector GlobalCoordinates(TLorentzVector pos){
 	TLorentzVector finalpos;
-	finalpos.SetXYZT(pos.X(),
-			pos.Y() -238.473,
-			pos.Z() +2391.0,
+	finalpos.SetXYZT(pos.X()*10,
+			pos.Y()*10 -2384.73,
+			pos.Z()*10 +23910,
 			pos.T());
 	return finalpos;
 }
