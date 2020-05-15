35b3a4b7bae732b9362159b82ceccffd24170ab3
Antonio Surdo <antonio.surdo@le.infn.it>
2020-05-15 18:12:02 +0200

    Update FillSegmentDetectors.h

--- fluka2edepsim/src/FillSegmentDetectors.h
+++ fluka2edepsim/src/FillSegmentDetectors.h
@@ -11,9 +11,10 @@ void SummarizeHitSegments(TG4HitSegmentContainer& dest, TTree *DetHits, int iev,
 
 	dest.clear();
 
-	const int NhitMax=MaxNhit;
-	const int NCellMax=MaxNhit;
-
+	//const int NhitMax=MaxNhit;
+	//const int NCellMax=MaxNhit;
+/*
+// moved to MainWrite.cxx:
 	//Int_t NStt;
 	Int_t IdStt[NhitMax];
 	Int_t IdParStt[NhitMax];
@@ -31,11 +32,13 @@ void SummarizeHitSegments(TG4HitSegmentContainer& dest, TTree *DetHits, int iev,
 //
 	Int_t NCells,IdCell[NCellMax],IdParCell[NCellMax];
 	Float_t PosCell[NCellMax][3],EdepCell[NCellMax],EdepQCell[NCellMax],TimeCell[NCellMax];
+*/
+    Int_t NCells;
     Float_t hitPosX,hitPosY,hitPosZ;
 //
     //std::cout<<"  ... in SummarizeHitSegments ... iev: "<<iev<<" ... Idet: "<<idet<<std::endl;
 		
-    if (idet < 3) {
+    if (idet < 4) {
 	DetHits->SetBranchAddress("NStt",&NStt);
 	DetHits->SetBranchAddress("IdStt",&IdStt);
 	DetHits->SetBranchAddress("IdParStt",&IdParStt);
@@ -54,14 +57,14 @@ void SummarizeHitSegments(TG4HitSegmentContainer& dest, TTree *DetHits, int iev,
 	DetHits->GetEntry(iev); // AS
 
         //if (entries>100) entries = 100;  // for debug
-	int det_str = 99;
+	std::string det_str = "what";
 
 	if (idet==1) {
 	    NSttHits = 0;
 	    for(int j=0; j<NStt; j++){
-		float rad2 = pow(PosOuStt[j][1],2) + pow(PosOuStt[j][2],2);
+		float rad2 = pow(PosInStt[j][1],2) + pow(PosInStt[j][2],2);
 // STT:
-		if (rad2 < 40000. && std::abs(PosOuStt[j][0])<169.) {
+		if (rad2 < 40000. && std::abs(PosInStt[j][0])<169.) {
 
 			TG4HitSegment hit;
 			TG4HitSegment::Contributors Contrib;
@@ -80,22 +83,28 @@ void SummarizeHitSegments(TG4HitSegmentContainer& dest, TTree *DetHits, int iev,
 			TLorentzVector pos_out(PosOuStt[j][0],PosOuStt[j][1],PosOuStt[j][2],TimeStt[j]);
 			
 			hit.Start=GlobalCoordinates(pos_in);
-        	hit.Stop=GlobalCoordinates(pos_out);
-			
-            hitPosX = 0.5*(hit.Start.X()+hit.Stop.X());
-            hitPosY = 0.5*(hit.Start.Y()+hit.Stop.Y());
-            hitPosZ = 0.5*(hit.Start.Z()+hit.Stop.Z());
-            TVector3 hitPos(hitPosX,hitPosY,hitPosZ);
-		
-			if (DirStt[j] == 1) det_str =1; // "stt_horizontal";
-			else if (DirStt[j] == 2) det_str =2; // "stt_vertical";
-			else {
-				std::cout<<" ERROR not found det_str : "<<DirStt[j]<<std::endl;
-				det_str= 10; // "not_found";
-			}
-			MapGeometry::Get()->AddPointToMap(det_str, hitPos);			
+        		hit.Stop=GlobalCoordinates(pos_out);
 
-    		NSttHits++;
+			hitPosX = 0.5*(hit.Start.X()+hit.Stop.X());
+			hitPosY = 0.5*(hit.Start.Y()+hit.Stop.Y());
+			hitPosZ = 0.5*(hit.Start.Z()+hit.Stop.Z());
+			TVector3 hitPos(hitPosX,hitPosY,hitPosZ);
+		
+			//std::string det_str = "what (stt)";
+			if (DirStt[j] == 1) det_str = "stt_horizontal";
+			else if (DirStt[j] == 2) det_str = "stt_vertical";
+			else {
+			  std::cout<<" ... ??? det_str ???: "<<DirStt[j]<<std::endl;
+			  std::cout<<" ... x: "<<PosInStt[j][0]<<"  y: "<<PosInStt[j][1]<<"  z: "<<PosInStt[j][2]<<" , rad2: "<<rad2<<std::endl;
+			}
+			
+		//cout<<" STT-hit: "<<NSttHits<<" DirStt: "<<DirStt[j]<<"  det: "<<det_str<<std::endl;
+
+			//MapGeometry::Get()->AddPointToMap("prova", hitstart);			
+			MapGeometry::Get()->AddPointToMap(det_str, hitPos);			
+			
+			TrSttHits[NSttHits] = TrStt[j];
+			NSttHits++;
 /*
             if (segLen == 0.0) {
 	            //std::cout<<" ... hit-Loop .. segLen: "<<segLen<<" ... j: "<<j<<std::endl;
@@ -109,11 +118,12 @@ void SummarizeHitSegments(TG4HitSegmentContainer& dest, TTree *DetHits, int iev,
 	    //std::cout<<" STT> NSttHits: "<<NSttHits<<std::endl;
 	}
 	else if (idet==2) {
-	    int NCalHits = 0;
+	    //int NCalHits = 0;
+	    NCalHits = 0;
 	    for(int j=0; j<NStt; j++){
-		float rad2 = pow(PosOuStt[j][1],2) + pow(PosOuStt[j][2],2);
+		float rad2 = pow(PosInStt[j][1],2) + pow(PosInStt[j][2],2);
 // ECAL:
-		if (((rad2>=40000.&&rad2<50625.) || (std::abs(PosOuStt[j][0])>=169.)) && std::abs(PosOuStt[j][0])<216.) {
+		if (((rad2>=40000.&&rad2<50625.) || (std::abs(PosInStt[j][0])>=169.)) && std::abs(PosInStt[j][0])<216.) {
 
 			TG4HitSegment hit;
 			TG4HitSegment::Contributors Contrib;
@@ -134,15 +144,23 @@ void SummarizeHitSegments(TG4HitSegmentContainer& dest, TTree *DetHits, int iev,
 			hit.Start=GlobalCoordinates(pos_in);
 	        	hit.Stop=GlobalCoordinates(pos_out);
 	
-            hitPosX = 0.5*(hit.Start.X()+hit.Stop.X());
-            hitPosY = 0.5*(hit.Start.Y()+hit.Stop.Y());
-            hitPosZ = 0.5*(hit.Start.Z()+hit.Stop.Z());
-            TVector3 hitPos(hitPosX,hitPosY,hitPosZ);
+			hitPosX = 0.5*(hit.Start.X()+hit.Stop.X());
+			hitPosY = 0.5*(hit.Start.Y()+hit.Stop.Y());
+			hitPosZ = 0.5*(hit.Start.Z()+hit.Stop.Z());
+			TVector3 hitPos(hitPosX,hitPosY,hitPosZ);
+		
+			//std::string det_str = "what (ecal)";
+			if (DirStt[j] == 0) det_str = "ecal";
+			else {
+			  std::cout<<" ... ??? det_str ???: "<<DirStt[j]<<std::endl;
+			  std::cout<<" ... x: "<<PosInStt[j][0]<<"  y: "<<PosInStt[j][1]<<"  z: "<<PosInStt[j][2]<<" , rad2: "<<rad2<<std::endl;
+			}
 
-			if (DirStt[j] == 0) det_str = 0; //"ecal";
-			else { std::cout<<" NOT FOunD ... ??? det_str ???: "<<DirStt[j]<<std::endl; det_str=9;}
+		//cout<<" CAL-hit: "<<NCalHits<<" DirStt: "<<DirStt[j]<<"  det: "<<det_str<<std::endl;
+
 			MapGeometry::Get()->AddPointToMap(det_str, hitPos);			
 
+			TrCalHits[NCalHits] = TrStt[j];
 			NCalHits++;
 /*
             if (segLen == 0.0) {
@@ -156,8 +174,61 @@ void SummarizeHitSegments(TG4HitSegmentContainer& dest, TTree *DetHits, int iev,
 	    }
 	   // std::cout<<" ECAL> NCalHits: "<<NCalHits<<std::endl;
 	}
+	else if (idet==3) {
+	    NCatchHits = 0;
+	    for(int j=0; j<NStt; j++){
+		float rad2 = pow(PosInStt[j][1],2) + pow(PosInStt[j][2],2);
+// Mu-Catcher:
+		if (rad2>50625. || std::abs(PosInStt[j][0])>216.) {
+
+			TG4HitSegment hit;
+			TG4HitSegment::Contributors Contrib;
+		
+			hit.PrimaryId = PrimTrStt[j];
+			hit.EnergyDeposit = EdepStt[j];
+			hit.SecondaryDeposit = 0.0;  //non ce lo abbiamo
+			float segLen = sqrt(pow((PosOuStt[j][0]-PosInStt[j][0]),2) + pow((PosOuStt[j][1]-PosInStt[j][1]),2) + pow((PosOuStt[j][2]-PosInStt[j][2]),2));
+			hit.TrackLength = segLen;
+
+			(hit.Contrib).push_back(TrStt[j]);
+
+			TLorentzVector pos_in(PosInStt[j][0],PosInStt[j][1],PosInStt[j][2],TimeStt[j]);
+			TLorentzVector pos_out(PosOuStt[j][0],PosOuStt[j][1],PosOuStt[j][2],TimeStt[j]);
+
+			hit.Start=GlobalCoordinates(pos_in);
+	        	hit.Stop=GlobalCoordinates(pos_out);
+	
+			hitPosX = 0.5*(hit.Start.X()+hit.Stop.X());
+			hitPosY = 0.5*(hit.Start.Y()+hit.Stop.Y());
+			hitPosZ = 0.5*(hit.Start.Z()+hit.Stop.Z());
+			TVector3 hitPos(hitPosX,hitPosY,hitPosZ);
+		
+			if (DirStt[j] == 0) det_str = "catcher";
+			else {
+			  std::cout<<" ... ??? det_str ???: "<<DirStt[j]<<std::endl;
+			  std::cout<<" ... x: "<<PosInStt[j][0]<<"  y: "<<PosInStt[j][1]<<"  z: "<<PosInStt[j][2]<<" , rad2: "<<rad2<<std::endl;
+			}
+
+		//cout<<" CAL-hit: "<<NCalHits<<" DirStt: "<<DirStt[j]<<"  det: "<<det_str<<std::endl;
+
+			MapGeometry::Get()->AddPointToMap(det_str, hitPos);			
+
+			TrCatchHits[NCatchHits] = TrStt[j];
+			NCatchHits++;
+/*
+            if (segLen == 0.0) {
+	            //std::cout<<" ... hit-Loop .. segLen: "<<segLen<<" ... j: "<<j<<std::endl;
+		  //	    std::cout<<" ... xin: "<<PosInStt[j][0]<<" yin: "<<PosInStt[j][1]<<" zin: "<<PosInStt[j][2]<<std::endl;
+		  //	    std::cout<<" ... xout: "<<PosOuStt[j][0]<<" yout: "<<PosOuStt[j][1]<<" zout: "<<PosOuStt[j][2]<<std::endl;
+	        }
+*/
+			dest.push_back(hit);
+		}
+	    }
+	   // std::cout<<" CATCHER> NCatchHits: "<<NCatchHits<<std::endl;
+	}
     }
-    else if (idet==3) {
+    else if (idet==4) {
 
 // 3DST:
 
@@ -191,14 +262,14 @@ void SummarizeHitSegments(TG4HitSegmentContainer& dest, TTree *DetHits, int iev,
 		TLorentzVector pos_out(PosCell[j][0],PosCell[j][1],PosCell[j][2],TimeStt[j]);
 
 		hit.Start=GlobalCoordinates(pos_in);
-        hit.Stop=GlobalCoordinates(pos_out);
+        	hit.Stop=GlobalCoordinates(pos_out);
 
-        hitPosX = 0.5*(hit.Start.X()+hit.Stop.X());
-        hitPosY = 0.5*(hit.Start.Y()+hit.Stop.Y());
-        hitPosZ = 0.5*(hit.Start.Z()+hit.Stop.Z());
-        TVector3 hitPos(hitPosX,hitPosY,hitPosZ);
-		int det_str=3;
-		MapGeometry::Get()->AddPointToMap(det_str, hitPos);			
+		hitPosX = 0.5*(hit.Start.X()+hit.Stop.X());
+		hitPosY = 0.5*(hit.Start.Y()+hit.Stop.Y());
+		hitPosZ = 0.5*(hit.Start.Z()+hit.Stop.Z());
+		TVector3 hitPos(hitPosX,hitPosY,hitPosZ);
+		
+		MapGeometry::Get()->AddPointToMap("3dst", hitPos);			
 
 		NDetHits++;
 		dest.push_back(hit);
@@ -212,22 +283,25 @@ void SummarizeHitSegments(TG4HitSegmentContainer& dest, TTree *DetHits, int iev,
 void FillSegmentDetectors(std::map<std::string, std::vector<TG4HitSegment> >& dest, TTree *SttHits, TTree *ScdHits,int ientry) {
 
 	dest.clear();
-	
+
 	std::string det = "Straw";
 	int iflag = 1;
 	int entries_stt = 0;
 	SummarizeHitSegments(dest[det], SttHits, ientry, entries_stt, iflag);
-
 //
 	det = "EMCalSci";
 	iflag = 2;
 	int entries_ecal = 0;
 	SummarizeHitSegments(dest[det], SttHits, ientry, entries_ecal, iflag);
-
-
-	det = "3DST";
+//
+	det = "Catcher";
 	iflag = 3;
+	int entries_ecatch = 0;
+	SummarizeHitSegments(dest[det], SttHits, ientry, entries_ecatch, iflag);
+//
+	det = "3DST";
+	iflag = 4;
 	int entries_3dst = 0;
 	SummarizeHitSegments(dest[det], ScdHits, ientry, entries_3dst, iflag);
-
+//
 }
